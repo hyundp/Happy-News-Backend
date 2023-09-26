5f45b7f301a788ea84682ffbbb8d67c0b14e826d
dyun <zkdlxkxks@nate.com>
2023-09-26 13:40:45 +0900

    update: news repo interface

--- src/main/java/com/example/happy/repository/NewsRepository.java
+++ src/main/java/com/example/happy/repository/NewsRepository.java
@@ -1,8 +1,14 @@
 package com.example.happy.repository;
 
-import com.example.happy.domain.News;
+import com.example.happy.model.News;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface NewsRepository extends MongoRepository<News, String>{
-
+    List<News> getRandomSample(int sampleSize);
+    List<News> getPickRandomSample(int sampleSize, String id);
+    List<News> getById(String id);
 }
