406d647bb4fb92883afd263784131009e603729f
dyun <zkdlxkxks@nate.com>
2023-09-26 13:41:03 +0900

    update: news service impl

--- src/main/java/com/example/happy/service/impl/NewsServiceImpl.java
+++ src/main/java/com/example/happy/service/impl/NewsServiceImpl.java
@@ -1,10 +1,19 @@
 package com.example.happy.service.impl;
 
-import com.example.happy.domain.News;
+import com.example.happy.model.News;
 import com.example.happy.repository.NewsRepository;
 import com.example.happy.service.NewsService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.aggregation.*;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class NewsServiceImpl implements NewsService {
@@ -15,27 +24,22 @@ public class NewsServiceImpl implements NewsService {
         this.newsRepository = newsRepository;
     }
     @Override
-    public News getId(int id) {
-        return null;
+    public List<News> getPickNews(String id) {
+        List<News> news_list = new ArrayList<>();
+
+        if (newsRepository.findById(id) == null) {
+            return null;
+        }
+        else{
+            news_list.addAll(newsRepository.getById(id));
+            news_list.addAll(newsRepository.getPickRandomSample(9, id));
+            return news_list;
+        }
     }
 
     @Override
-    public News getLink(String link) {
-        return null;
+    public List<News> getAllNews(){
+        return newsRepository.getRandomSample(10);
     }
 
-    @Override
-    public News getTitle(String title) {
-        return newsRepository.findById(title).orElse(null);
-    }
-
-    @Override
-    public News getSummary(String summary) {
-        return null;
-    }
-
-    @Override
-    public News getProb(float prob) {
-        return null;
-    }
 }
